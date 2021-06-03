1923e279355b58043cff55556bee479332d9855c
samsonite98765 <77023084+samsonite98765@users.noreply.github.com>
2021-06-03 13:12:23 -0400

    Create NUGN.cpp

---
+++ src/simulation/elements/NUGN.cpp
@@ -0,0 +1,46 @@
+#include "simulation/ElementCommon.h"
+
+static int update(UPDATE_FUNC_ARGS);
+
+void Element::Element_NUGN()
+{
+	Identifier = "DEFAULT_PT_NUGN";
+	Name = "NUGN";
+	Colour = PIXPACK(0x5070FF);
+	MenuVisible = 1;
+	MenuSection = SC_GAS;
+	Enabled = 1;
+
+	Advection = 2.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.99f;
+	Loss = 0.30f;
+	Collision = -0.10f;
+	Gravity = 0.00f;
+	Diffusion = 3.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 100000000000;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 0;
+
+	Weight = 1;
+
+	HeatConduct = 251;
+	Description = "nukedragin.";
+
+	Properties = TYPE_GAS;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+
+}
+
