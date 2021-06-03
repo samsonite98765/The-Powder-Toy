0e6704275749d45e88d191a5519b8d4270dd86ef
samsonite98765 <77023084+samsonite98765@users.noreply.github.com>
2021-06-03 13:09:27 -0400

    Create rnd2.cpp

---
+++ src/simulation/elements/rnd2.cpp
@@ -0,0 +1,42 @@
+#include "simulation/ElementCommon.h"
+
+void Element::Element_RND2()
+{
+	Identifier = "DEFAULT_PT_RND2";
+	Name = "RND2";
+	Colour = PIXPACK(0x404066);
+	MenuVisible = 1;
+	MenuSection = SC_SPECIAL;
+	Enabled = 1;
+
+	Advection = 0.7f;
+	AirDrag = 0.02f * CFDS;
+	AirLoss = 0.96f;
+	Loss = 0.80f;
+	Collision = 0.0f;
+	Gravity = 0.1f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 1;
+
+	Flammable = 1;
+	Explosive = 0;
+	Meltable = 1;
+	Hardness = 1;
+
+	Weight = 100;
+
+	HeatConduct = 251;
+	Description = "The basic random conductor. Meltable.";
+
+	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC|PROP_HOT_GLOW;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 1273.0f;
+	HighTemperatureTransition = PT_LAVA;
+}
