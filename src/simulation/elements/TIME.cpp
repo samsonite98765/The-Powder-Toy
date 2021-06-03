d3f51fb9e207b2f47e4534fbfb492af8cd8df4eb
samsonite98765 <77023084+samsonite98765@users.noreply.github.com>
2021-06-03 13:12:49 -0400

    Create TIME.cpp

---
+++ src/simulation/elements/TIME.cpp
@@ -0,0 +1,46 @@
+#include "simulation/ElementCommon.h"
+
+
+void Element::Element_TIME()
+{
+	Identifier = "DEFAULT_PT_TIME";
+	Name = "TIME";
+	Colour = PIXPACK(0x505070);
+	MenuVisible = 1;
+	MenuSection = SC_SOLIDS;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.90f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 1;
+	Hardness = 1;
+
+	Weight = 100;
+
+	HeatConduct = 251;
+	Description = "a timer(set life)";
+
+	Properties = TYPE_SOLID|PROP_LIFE_DEC|PROP_LIFE_KILL|PROP_CONDUCTS|PROP_LIFE_DEC|PROP_HOT_GLOW;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = 1.0f;
+	HighPressureTransition = ST;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 1273.0f;
+	HighTemperatureTransition = PT_LAVA;
+
+	
+}
+
